a08b5bb22c73eea9267ab26b10865fb3b12cfbbe
nimarahnamadev1996 <nimarahnamadev1996@gmail.com>
2025-10-16 12:33:57 +0330

    install project - dark and light- navbar responsive

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,69 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { ClerkProvider }  from '@clerk/nextjs'
+
+
+import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "ComfyZone",
+  description: "users can share posts, like, comment, and follow others in a friendly and comfy space.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+     <ClerkProvider>
+       <html lang="en">
+          <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange>
+
+                <div className="min-h-screen">
+                  <Navbar/>
+
+                  <main className="py-8">
+
+                    <div className="max-w-7xl mx-auto px-4">
+                      <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
+                        
+                        <div className="hidden lg:col-span-3 lg:block">
+                           <Sidebar/>
+                        </div>
+
+                        <div className="lg:col-span-9">
+                          {children}
+                        </div>
+                      </div>
+                    </div>
+                  </main>
+                </div>
+                {children}
+            </ThemeProvider>
+          </body>
+       </html>
+     </ClerkProvider>
+  );
+}
